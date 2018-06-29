66cf888324708816da2cfa2d472c2a8d39acc445
Joe Woznicki <gapwoz@gmail.com>
2018-06-28 21:35:02 -0500

    Added crypto.js file and changed serenity bot api to my tester bot

--- config/config.js
+++ config/config.js
@@ -10,6 +10,8 @@ if(env === 'development' || env === 'test') {
 
   process.env["SERENITY_BOT"] = config["SERENITY_BOT"];
   process.env["STEAM_API_KEY"] = config["STEAM_API_KEY"];
+  process.env["BINANCE_API_KEY"] = config["STEAM_API_KEY"];
+  process.env["BINANCE_API_SECRET"] = config["STEAM_API_KEY"];
   process.env["OPEN_WEATHER_API_KEY"] = config["OPEN_WEATHER_API_KEY"];
   process.env["DEVELOPMENT_BOT"] = config["DEVELOPMENT_BOT"];
 }
